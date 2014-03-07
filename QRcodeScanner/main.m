1afc9c5d1be0d14551b6fae88553104e878e9ff4
Neeraj Rathi <neerajrathi@Neerajs-Mac-mini.local>
2014-03-07 11:02:37 +0530

    Initial Commit

---
+++ QRcodeScanner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QRcodeScanner
+//
+//  Created by Neeraj Rathi on 07/03/14.
+//  Copyright (c) 2014 Neeraj Rathi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ QRcodeScannerTests/QRcodeScannerTests.m
@@ -0,0 +1,34 @@
+//
+//  QRcodeScannerTests.m
+//  QRcodeScannerTests
+//
+//  Created by Neeraj Rathi on 07/03/14.
+//  Copyright (c) 2014 Neeraj Rathi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface QRcodeScannerTests : XCTestCase
+
+@end
+
+@implementation QRcodeScannerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
